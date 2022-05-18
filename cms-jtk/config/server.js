e17858c09e2de4afac77e9885164e2bb127ea3f8
Azzambz <azzambz17@gmail.com>
2022-05-18 18:55:41 +0700

    change database in strapi

--- cms-jtk/config/server.js
+++ cms-jtk/config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9de7a952cb1020c0c1ee3283eacc69d1'),
+    },
   },
 });
---
+++ cms-jtk/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1884ccca-00e8-44c5-a605-0348ba76378d'
+};
