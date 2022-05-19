18f60dac0a99f3b588f886bffd5e89dae1ed53e6
wafikhn <54474529+wafikhn@users.noreply.github.com>
2022-05-19 22:03:52 +0700

    article get data strapi
    blabla

--- frontend/src/components/ListArticle.vue
+++ frontend/src/components/ListArticle.vue
@@ -10,114 +10,38 @@
 <section class="text-gray-600 body-font">
         <div class="container px-5 pt-10 mx-auto mb-10">
 
-        <div>
-        <h1 class=" text-white text-4xl mb-10 font-serif">
-          Artikel
-        </h1>
-      </div>
-          <div class="flex flex-wrap -m-4">
+      
+          <div class="flex flex-wrap -m-4"
+           >
+            
 
+              
+            
+    
 
-            <div class="p-4 md:w-1/3">
+            <div class="p-4 md:w-1/3"
+            v-for="(tag,index) in article"
+           :key="tag">
               <div class="h-full rounded shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
-                <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog">
+                <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"  v-bind:src="article[index].article_medias[0].media.url">
                 <div class="p-6">
 
-                  <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
-                  <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                  <h1 class="title-font text-lg font-medium text-gray-600 mb-3">{{tag.judul}}</h1>
+                  <p class="leading-relaxed mb-3">{{tag.deskripsi.slice(0,100)}}</p>
+
                   <div class="flex items-center flex-wrap ">
-                    <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
-                   
+                    <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg outline-second">Learn more</button>
                   </div>
                 </div>
               </div>
             </div>
 
-           <div class="p-4 md:w-1/3">
-              <div class="h-full rounded shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
-                <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog">
-                <div class="p-6">
+            
 
-                  <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
-                  <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                  <div class="flex items-center flex-wrap ">
-                    <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
-                   
-                  </div>
-                </div>
-              </div>
-            </div>
-
-              <div class="p-4 md:w-1/3">
-              <div class="h-full rounded shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
-                <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog">
-                <div class="p-6">
-
-                  <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
-                  <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                  <div class="flex items-center flex-wrap ">
-                    <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
-                   
-                  </div>
-                </div>
-              </div>
-            </div>
               
           </div>
         </div>
 
-        <div class="container px-5 py-2  mx-auto">
-          <div class="flex flex-wrap -m-4">
-
-
-          <div class="p-4 md:w-1/3">
-              <div class="h-full rounded shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
-                <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog">
-                <div class="p-6">
-
-                  <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
-                  <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                  <div class="flex items-center flex-wrap ">
-                    <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
-                   
-                  </div>
-                </div>
-              </div>
-            </div>
-
-             <div class="p-4 md:w-1/3">
-              <div class="h-full rounded shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
-                <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog">
-                <div class="p-6">
-
-                  <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
-                  <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                  <div class="flex items-center flex-wrap ">
-                    <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
-                   
-                  </div>
-                </div>
-              </div>
-            </div>
-
-               <div class="p-4 md:w-1/3">
-              <div class="h-full rounded shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
-                <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog">
-                <div class="p-6">
-
-                  <h1 class="title-font text-lg font-medium text-gray-600 mb-3">The Catalyzer</h1>
-                  <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-                  <div class="flex items-center flex-wrap ">
-                    <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Learn more</button>
-                   
-                  </div>
-                </div>
-              </div>
-            </div>
-            
-
-          </div>
-        </div>
 
         
 
@@ -153,15 +77,25 @@
 <script lang="ts">
 import Vue from "vue";
 import Footer from "./Footer.vue"
+import axios from 'axios';
 
 export default Vue.extend({
     components:{Footer},
 
   data() {
     return {
-
+      article : []
     };
   },
+  mounted(){
+        axios
+            .get('Articles')
+            .then(response => {
+                this.article = response.data
+                // console.log(response.data)
+            })
+            .catch(error => console.log(error))
+}
 
 });
 </script>
