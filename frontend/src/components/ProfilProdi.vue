bffa0aa25314668e3d5ef9a00bd5c85e03b71f11
Herlana <hrlana1312@gmail.com>
2022-05-20 11:06:17 +0700

    Add Navbar

--- frontend/src/components/ProfilProdi.vue
+++ frontend/src/components/ProfilProdi.vue
@@ -1,177 +1,247 @@
 <template>
-<div class="bg-JTK ">
+  <div class="bg-JTK">
+    <Navbar />
 
-<div class="w-full pt-10 mb-10">
-  <img class=" w-full h-96" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog">
-</div>
-                
+    <div class="w-full pt-10 mb-10">
+      <img
+        class="w-full h-96"
+        src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+        alt="blog"
+      />
+    </div>
 
-<hr class="mb-10">
+    <hr class="mb-10" />
 
+    <div class="container mx-auto">
+      <!-- Body Gan -->
+      <div>
+        <div class="flex flex-wrap">
+          <!-- Bagian Left Side Gan -->
 
-<div class="container mx-auto">    
-        
-                        <!-- Body Gan -->
-                        <div>
-                            <div class=" flex flex-wrap ">
+          <div class="lg:w-3/4 sm:w-full lg:pr-20 sm:pr-0 ...">
+            <h1 class="text-white font-semibold text-5xl mt-4 mb-24">Prodi</h1>
+            <div class="grid grid-cols-8">
+              <div class="col-start-3 col-end-9 ml-4">
+                <div class="flex flex-row">
+                  <button class="bg-second p-2 px-4 mr-4">
+                    <p class="text-white text-center">Visi misi</p>
+                  </button>
+                  <button class="bg-second p-2 px-4 mr-4">
+                    <p class="text-white text-center">Profil Jurusan</p>
+                  </button>
+                  <button class="bg-second p-2 px-4">
+                    <p class="text-white text-center">Keungggulan</p>
+                  </button>
+                </div>
+              </div>
 
-                                <!-- Bagian Left Side Gan -->
-                              
-                                <div class="  lg:w-3/4 sm:w-full lg:pr-20 sm:pr-0 ... ">
-                                  
-                                    <h1 class=" text-white font-semibold text-5xl mt-4 mb-24">Prodi</h1>
-                                    <div class=" grid grid-cols-8  ">
+              <div class="col-start-1 col-end-3 mt-4">
+                <div class="">
+                  <button class="bg-second p-2 mr-4 w-full mb-4">
+                    <p class="text-white text-center">
+                      D3 - Teknik Informatika
+                    </p>
+                  </button>
+                  <button class="bg-second p-2 mr-4 w-full mb-4">
+                    <p class="text-white text-center">
+                      D4 - Teknik Informatika
+                    </p>
+                  </button>
+                </div>
+              </div>
 
-
-                                        <div class=" col-start-3 col-end-9 ml-4  ">
-                                            <div class=" flex flex-row">
-                                                <button class=" bg-second p-2 px-4 mr-4 ">
-                                                <p class=" text-white text-center">Visi misi</p>
-                                            </button>
-                                            <button class=" bg-second p-2 px-4 mr-4 ">
-                                                <p class=" text-white text-center">Profil Jurusan</p>
-                                            </button>
-                                             <button class=" bg-second p-2 px-4 ">
-                                                <p class=" text-white text-center">Keungggulan</p>
-                                            </button>
-                                            </div>
-                                        </div>
-
-
-                                            <div class=" col-start-1 col-end-3 mt-4">
-                                                <div class="">
-                                                <button class=" bg-second p-2 mr-4 w-full mb-4 ">
-                                                <p class=" text-white text-center">D3 - Teknik Informatika</p>
-                                            </button>
-                                            <button class=" bg-second p-2 mr-4 w-full mb-4 ">
-                                                <p class=" text-white text-center">D4 - Teknik Informatika</p>
-                                            </button>
-  
-                                            </div>
-
-                                            </div>
-
-                                            <div class=" col-start-3 col-end-9  border-4">
-                                                <h1 class=" text-white p-4 text-4xl">
-                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, dolores? Deleniti omnis repellendus, placeat eius officia similique dolores adipisci, eos reiciendis delectus esse mollitia modi reprehenderit quos odio minima eligendi?
-                                                </h1>
-                                            </div>
-
-                                
-
-                                       
-                                        
-
-                                        
-
-                            
-                                       
-
-                                        
-
-                                    </div>
-                                </div>
-
-                  
-
-                                  <!-- Bagian Right Side Gan ------------------------------------------------------------------------------------------------------------------------------------------------>
-                                    <div class=" lg:w-1/4 sm:w-full  ...">
-                                            <section class="mb-4">
-                                              <div class="border-l-2  pl-4">
-                                                <h1 class=" text-white flex flex-wrap font-semibold text-2xl pt-5 mb-10 " >Artikel Lainnya</h1>
-                                                  
-                                                  <div class="flex mb-10 bg-white">
-                                                           <img class=" mr-4 min-h-0 min-w-0 float-left rounded-md" src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog">
-                                                          <!-- <img style="width:100%" :src="userProfilePic.banner_img[0].url" alt=""> -->
-                                                          <div>
-                                                              <h2 class="   text-indigo-900 tracking-widest text-md title-font font-medium text-gray-400 mb-1">Ingin Liburan Seru Dengan Tour Travel? Coba 7 Tips Memilih Jasa Tour Travel untuk Liburan Keluarga</h2>
-                                                          </div>
-                                                    </div>
-
-                                                    <div class="flex mb-10 bg-white">
-                                                          <img class=" mr-4 min-h-0 min-w-0 float-left rounded-md" src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog">
-                                                          <!-- <img style="width:100%" :src="userProfilePic.banner_img[0].url" alt=""> -->
-                                                          <div>
-                                                              <h2 class="   text-indigo-900 tracking-widest text-md title-font font-medium text-gray-400 mb-1">Ingin Liburan Seru Dengan Tour Travel? Coba 7 Tips Memilih Jasa Tour Travel untuk Liburan Keluarga</h2>
-                                                          </div>
-                                                    </div>
-
-                                                    <div class="flex mb-10 bg-white">
-                                                           <img class=" mr-4 min-h-0 min-w-0 float-left rounded-md" src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog">
-                                                          <!-- <img style="width:100%" :src="userProfilePic.banner_img[0].url" alt=""> -->
-                                                          <div>
-                                                              <h2 class="   text-indigo-900 tracking-widest text-md title-font font-medium text-gray-400 mb-1">Ingin Liburan Seru Dengan Tour Travel? Coba 7 Tips Memilih Jasa Tour Travel untuk Liburan Keluarga</h2>
-                                                          </div>
-                                                    </div>
-
-                                                     <div class="flex mb-10 bg-white">
-                                                            <img class=" mr-4 min-h-0 min-w-0 float-left rounded-md" src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog">
-                                                          <!-- <img style="width:100%" :src="userProfilePic.banner_img[0].url" alt=""> -->
-                                                          <div>
-                                                              <h2 class="   text-indigo-900 tracking-widest text-md title-font font-medium text-gray-400 mb-1">Ingin Liburan Seru Dengan Tour Travel? Coba 7 Tips Memilih Jasa Tour Travel untuk Liburan Keluarga</h2>
-                                                          </div>
-                                                    </div>
-
-                                                    <div class="flex mb-10 bg-white">
-                                                           <img class=" mr-4 min-h-0 min-w-0 float-left rounded-md" src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60" alt="blog">
-                                                          <!-- <img style="width:100%" :src="userProfilePic.banner_img[0].url" alt=""> -->
-                                                          <div>
-                                                              <h2 class="   text-indigo-900 tracking-widest text-md title-font font-medium text-gray-400 mb-1">Ingin Liburan Seru Dengan Tour Travel? Coba 7 Tips Memilih Jasa Tour Travel untuk Liburan Keluarga</h2>
-                                                          </div>
-                                                    </div>
-
-
-                                              </div>
-                                            </section>
-
-                
-
-
-                                    </div>
-
-                            </div>
+              <div class="col-start-3 col-end-9 border-4">
+                <h1 class="text-white p-4 text-4xl">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quisquam, dolores? Deleniti omnis repellendus, placeat eius
+                  officia similique dolores adipisci, eos reiciendis delectus
+                  esse mollitia modi reprehenderit quos odio minima eligendi?
+                </h1>
+              </div>
             </div>
+          </div>
+
+          <!-- Bagian Right Side Gan ------------------------------------------------------------------------------------------------------------------------------------------------>
+          <div class="lg:w-1/4 sm:w-full ...">
+            <section class="mb-4">
+              <div class="border-l-2 pl-4">
+                <h1
+                  class="
+                    text-white
+                    flex flex-wrap
+                    font-semibold
+                    text-2xl
+                    pt-5
+                    mb-10
+                  "
+                >
+                  Artikel Lainnya
+                </h1>
+
+                <div class="flex mb-10 bg-white">
+                  <img
+                    class="mr-4 min-h-0 min-w-0 float-left rounded-md"
+                    src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60"
+                    alt="blog"
+                  />
+                  <!-- <img style="width:100%" :src="userProfilePic.banner_img[0].url" alt=""> -->
+                  <div>
+                    <h2
+                      class="
+                        text-indigo-900
+                        tracking-widest
+                        text-md
+                        title-font
+                        font-medium
+                        text-gray-400
+                        mb-1
+                      "
+                    >
+                      Ingin Liburan Seru Dengan Tour Travel? Coba 7 Tips Memilih
+                      Jasa Tour Travel untuk Liburan Keluarga
+                    </h2>
+                  </div>
+                </div>
+
+                <div class="flex mb-10 bg-white">
+                  <img
+                    class="mr-4 min-h-0 min-w-0 float-left rounded-md"
+                    src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60"
+                    alt="blog"
+                  />
+                  <!-- <img style="width:100%" :src="userProfilePic.banner_img[0].url" alt=""> -->
+                  <div>
+                    <h2
+                      class="
+                        text-indigo-900
+                        tracking-widest
+                        text-md
+                        title-font
+                        font-medium
+                        text-gray-400
+                        mb-1
+                      "
+                    >
+                      Ingin Liburan Seru Dengan Tour Travel? Coba 7 Tips Memilih
+                      Jasa Tour Travel untuk Liburan Keluarga
+                    </h2>
+                  </div>
+                </div>
+
+                <div class="flex mb-10 bg-white">
+                  <img
+                    class="mr-4 min-h-0 min-w-0 float-left rounded-md"
+                    src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60"
+                    alt="blog"
+                  />
+                  <!-- <img style="width:100%" :src="userProfilePic.banner_img[0].url" alt=""> -->
+                  <div>
+                    <h2
+                      class="
+                        text-indigo-900
+                        tracking-widest
+                        text-md
+                        title-font
+                        font-medium
+                        text-gray-400
+                        mb-1
+                      "
+                    >
+                      Ingin Liburan Seru Dengan Tour Travel? Coba 7 Tips Memilih
+                      Jasa Tour Travel untuk Liburan Keluarga
+                    </h2>
+                  </div>
+                </div>
+
+                <div class="flex mb-10 bg-white">
+                  <img
+                    class="mr-4 min-h-0 min-w-0 float-left rounded-md"
+                    src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60"
+                    alt="blog"
+                  />
+                  <!-- <img style="width:100%" :src="userProfilePic.banner_img[0].url" alt=""> -->
+                  <div>
+                    <h2
+                      class="
+                        text-indigo-900
+                        tracking-widest
+                        text-md
+                        title-font
+                        font-medium
+                        text-gray-400
+                        mb-1
+                      "
+                    >
+                      Ingin Liburan Seru Dengan Tour Travel? Coba 7 Tips Memilih
+                      Jasa Tour Travel untuk Liburan Keluarga
+                    </h2>
+                  </div>
+                </div>
+
+                <div class="flex mb-10 bg-white">
+                  <img
+                    class="mr-4 min-h-0 min-w-0 float-left rounded-md"
+                    src="https://images.unsplash.com/photo-1631700611307-37dbcb89ef7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60"
+                    alt="blog"
+                  />
+                  <!-- <img style="width:100%" :src="userProfilePic.banner_img[0].url" alt=""> -->
+                  <div>
+                    <h2
+                      class="
+                        text-indigo-900
+                        tracking-widest
+                        text-md
+                        title-font
+                        font-medium
+                        text-gray-400
+                        mb-1
+                      "
+                    >
+                      Ingin Liburan Seru Dengan Tour Travel? Coba 7 Tips Memilih
+                      Jasa Tour Travel untuk Liburan Keluarga
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
       </div>
+    </div>
 
-<Footer />
-</div>
-
+    <Footer />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Footer from "./Footer.vue"
-import axios from 'axios';
-
-
+import Footer from "./Footer.vue";
+import Navbar from "./Navbar.vue";
+import axios from "axios";
 
 export default Vue.extend({
-    components:{Footer},
+  components: { Footer, Navbar },
 
   data() {
     return {
-      article : [], 
+      article: [],
       function() {
-        const lang = localStorage.getItem('lang') || 'en';
-        return{
-          lang : lang
-        }
-      }
+        const lang = localStorage.getItem("lang") || "en";
+        return {
+          lang: lang,
+        };
+      },
     };
   },
-  mounted(){
-        axios
-            .get('languages?_locale=en')
-            .then(response => {
-                this.article = response.data
-                // console.log(response.data)
-            })
-            .catch(error => console.log(error))
-},
-methods:{
-
-}
-
+  mounted() {
+    axios
+      .get("languages?_locale=en")
+      .then((response) => {
+        this.article = response.data;
+        // console.log(response.data)
+      })
+      .catch((error) => console.log(error));
+  },
+  methods: {},
 });
-
 </script>
