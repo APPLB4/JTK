2ff4a5ae9aeb506663fc88af6e8bc6fd5855b1bf
wafikhn <54474529+wafikhn@users.noreply.github.com>
2022-05-26 19:50:59 +0700

    prestasi
    strapipipipipipi

--- frontend/src/components/Prestasi.vue
+++ frontend/src/components/Prestasi.vue
@@ -7,59 +7,109 @@
       </div>
 
       <div class="flex flex-row">
-        <button class="bg-second px-8 mr-4 b-height">
+        <button class="bg-second px-8 mr-4 b-height"  @click="test = cat[0].category">
           <p class="text-white text-center mb-0">Internasional</p>
         </button>
         <button class="bg-second px-8 mr-4">
-          <p class="text-white text-center mb-0">Regional</p>
+          <p class="text-white text-center mb-0" @click="test = cat[3].category">Regional</p>
         </button>
         <button class="bg-second px-8 mr-4">
-          <p class="text-white text-center mb-0">Nasional</p>
+          <p class="text-white text-center mb-0" @click="test = cat[1].category">Nasional</p>
         </button>
         <button class="bg-second px-8 mr-4">
-          <p class="text-white text-center mb-0">Prestasi</p>
+          <p class="text-white text-center mb-0" @click="test = cat[2].category">Provinsi</p>
         </button>
         <button class="bg-second px-8 mr-4">
-          <p class="text-white text-center mb-0">Lain lain</p>
+          <p class="text-white text-center mb-0" @click="test = cat[4].category">Lain lain</p>
         </button>
       </div>
 
-      <div class="border-4 mb-10">
-        <div class="my-10">
-          <img
-            class="m-auto w-1/2 rounded-md"
-            src="/img/juara.jpg"
-            alt="blog"
-          />
-        </div>
 
-        <div class="m-10">
-          <h1 class="text-white text-xl text-justify">
-            Sabtu (14/11/2020), Politeknik Negeri Bandung menggelar Sidang
-            terbuka senat Politeknik Negeri Bandung Wisuda Program Magister
-            Terapan, Sarjana Terapan, dan Diploma Tahun Akademik 2019/2020.
-            Wisuda diselenggarakan secara offline di Pendopo Tony Seowandito dan
-            online melalui media Youtube sebagai antisipasi penyebaran virus
-            covid-19. Berikut merupakan video live-streaming wisuda Polban 2020.
-            Acara dimulai pada 8.50 WIB dan berakhir pukul 12.00 WIB. Pada
-            wisuda tahun ini JTK berhasil meraih gelar wisudawan terbaik D3
-            se-Polban berkat M Wahyu Maulana Akbar (171511019).
-          </h1>
-          <h1 class="text-white text-xl text-justify">
-            Wisudawan dari Jurusan Teknik Komputer dan Informatika mengikuti
-            acara ramah tamah mulai pukul 13.00 WIB yang diselenggarakan oleh
-            manejemen JTK lewat aplikasi zoom. Kegiatan tersebut diisi dengan
-            sambutan manajemen, wisudawan terbaik, serta industri. Selanjutnya
-            dilaksanakan pula sesi tanya jawab dan penyampaian kesan dan pesan
-            oleh wisudawan dari JTK. Wisuda tahun 2020 ini adalah wisuda ke 30
-            tahun, setelah JTK pindah ke kampus Ciwaruga. Sebelumnya JTK,
-            beraada di kampus Ganesha, dibawah binaan ITB. Harapan kita semua
-            agar JTK semakin dapat menjalankan amanahnya sebagai institusi
-            pendidikan yang cukup tua. Selamat kepada wisudawan D3 dan D4 JTK
-            Polban ! Semoga ilmu yang telah didapatkan selama kuliah di JTK
-            dapat menjadi bekal untuk melangkah lebih jauh ke puncak kesuksesan
-          </h1>
+  
+
+     
+      <div class="border-4 mb-10 flex flex-wrap -m-4 ">
+        <div
+        v-for="(tag,index) in prestasi"
+        :key="tag" >
+
+        <div
+        v-show="test == tag.prestasi_category.category" class="p-4 md:w-full" >
+
+        
+
+        
+        <div class=" my-10">
+
+           <router-link
+                    :to="{
+                      name: 'ArticleRead',
+                      params: {
+                        id: index,
+                        judul: 'Prestasi',
+                        image: tag.media.url,
+                        deskripsi: tag.deskripsi,
+                      },
+                    }"
+                  >
+              <div
+                class="
+                  h-full
+                  rounded
+                  shadow-cla-blue
+                  bg-gradient-to-r
+                  from-indigo-50
+                  to-blue-50
+                  overflow-hidden
+                "
+              >
+                <img
+                  class="
+                    lg:h-48
+                    md:h-36
+                    w-96
+                    object-cover object-center
+                    scale-110
+                    transition-all
+                    duration-400
+                    hover:scale-100
+                  "
+                  v-bind:src="tag.media.url"
+                />
+                <div class="p-6">
+                  <h1 class="title-font text-lg font-medium text-gray-600 mb-3">
+                    {{ tag.judul }}
+                  </h1>
+
+                  <div class="flex items-center flex-wrap">
+                    <button
+                      class="
+                        bg-gradient-to-r
+                        from-cyan-400
+                        to-blue-400
+                        hover:scale-105
+                        drop-shadow-md
+                        shadow-cla-blue
+                        px-4
+                        py-1
+                        rounded-lg
+                        outline-second
+                      "
+                    >
+                     {{tag.prestasi_category.category}}
+                    </button>
+                  </div>
+                </div>
+              </div>
+           </router-link>
+           </div>
+
+       
+
         </div>
+      </div>
+
+        
       </div>
     </div>
 
@@ -77,12 +127,39 @@
 import Vue from "vue";
 import Footer from "./Footer.vue";
 import Navbar from "./Navbar.vue";
+import axios from "axios";
 
 export default Vue.extend({
   components: { Footer, Navbar },
 
   data() {
-    return {};
+    return {
+      prestasi : [],
+      image : [],
+      judul : [],
+      cat : [],
+      test : []
+    };
   },
+  mounted() {
+    axios
+      .get("prestasis")
+      .then((response) => {
+        this.prestasi = response.data;
+        // console.log(response.data)
+      })
+      .catch((error) => console.log(error));
+       axios
+      .get("prestasi-categories")
+      .then((response) => {
+        this.cat = response.data;
+        // console.log(response.data)
+      })
+      .catch((error) => console.log(error));
+  },
+  methods: {
+  
+  
+  }
 });
 </script>
